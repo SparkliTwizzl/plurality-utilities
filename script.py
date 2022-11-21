f2381d66f661efdf4c18a8fd2b70e264fa0bc1f9
SparkliTwizzl <github@sparklitwizzl.com>
2022-11-20 20:31:27 -0700

    make script read data file and print contents

--- script.py
+++ script.py
@@ -1,17 +1,24 @@
-# data and output files are referenced via paths stored in local files
-# local files are ignored in Git for privacy
+def main():
+    # data and output files are referenced via paths stored in local files
+    # local files are ignored in Git for privacy
 
-dataPathFile = open('data.akfpath', 'rt')
-dataPath = dataPathFile.readline()
-dataPathFile.close()
+    dataPathFile = open('data.akfpath', 'rt')
+    dataPath = dataPathFile.readline()
+    dataPathFile.close()
 
-dataFile = open(dataPath, 'rt')
-data = dataFile.readlines()
-dataFile.close()
+    dataFile = open(dataPath, 'rt')
+    data = dataFile.readlines()
+    dataFile.close()
 
-outputPathFile = open('output.akfpath', 'rt')
-outputPath = outputPathFile.readline()
-outputPathFile.close()
+    outputPathFile = open('output.akfpath', 'rt')
+    outputPath = outputPathFile.readline()
+    outputPathFile.close()
 
-with open(outputPath, 'wt') as outputFile:
-    
+    with open(outputPath, 'wt') as outputFile:
+        print(data)
+        
+
+# def parseEntry(data, outputFile):
+
+
+main()
