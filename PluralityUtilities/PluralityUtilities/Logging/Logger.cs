6b7e48879f969a1cea2337599f0df05aae389ac0
SparkliTwizzl <github@sparklitwizzl.com>
2022-12-15 05:17:38 -0700

    - create C# project - draft logging utility - update gitignore

---
+++ PluralityUtilities/PluralityUtilities/Logging/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace PluralityUtilities.Logging
+{
+	public static class Logger
+	{
+		private static StreamWriter _logFile;
+		private static readonly string _logFileName = ".\\";
+		private static string _logFilePath;
+
+
+		static Logger()
+		{
+			_logFilePath = _logFileName + DateTime.Now + ".log";
+			_logFile = new StreamWriter(_logFilePath);
+		}
+
+		public static void Log<T>(T message)
+		{
+			_logFile.Write(message.ToString());
+		}
+
+		public static void LogLine<T>(T message)
+		{
+			_logFile.WriteLine(message.ToString());
+		}
+	}
+}
---
+++ PluralityUtilities/PluralityUtilities/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PluralityUtilities
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
