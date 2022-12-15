5b1cb7a1d56919d16852f2518694b712ab2853e2
SparkliTwizzl <github@sparklitwizzl.com>
2022-12-15 05:26:52 -0700

    - fix log file name format - change log methods to not be templated

--- PluralityUtilities/PluralityUtilities/Logging/Logger.cs
+++ PluralityUtilities/PluralityUtilities/Logging/Logger.cs
@@ -6,24 +6,24 @@ namespace PluralityUtilities.Logging
 	public static class Logger
 	{
 		private static StreamWriter _logFile;
-		private static readonly string _logFileName = ".\\";
+		private static readonly string _logFileName = ".\\PluralityUtilities";
 		private static string _logFilePath;
 
 
 		static Logger()
 		{
-			_logFilePath = _logFileName + DateTime.Now + ".log";
+			_logFilePath = _logFileName + DateTime.Now.ToString("_yyyy-MM-dd_HH-mm-ss") + ".log";
 			_logFile = new StreamWriter(_logFilePath);
 		}
 
-		public static void Log<T>(T message)
+		public static void Log(string message)
 		{
-			_logFile.Write(message.ToString());
+			_logFile.Write(message);
 		}
 
-		public static void LogLine<T>(T message)
+		public static void LogLine(string message)
 		{
-			_logFile.WriteLine(message.ToString());
+			_logFile.WriteLine();
 		}
 	}
 }
