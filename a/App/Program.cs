178c552282d43ca7165723fe11d5b9d7c36ac1d0
SparkliTwizzl <github@sparklitwizzl.com>
2022-12-19 04:02:41 -0700

    create fresh project to solve build headaches (needs original project files to be re-added)

---
+++ a/App/Program.cs
@@ -0,0 +1,25 @@
+﻿#if RELEASE
+using System.Diagnostics;
+using System.IO;
+#endif
+using System.Reflection;
+
+using PluralityUtilities.Logging;
+
+
+namespace PluralityUtilities.App
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if DEBUG
+			Log.SetLogFolder(SolutionFolders.SolutionDir + "/run/log/");
+#elif RELEASE
+			Log.SetLogFolder(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/" + Process.GetCurrentProcess().ProcessName + "_log/");
+#endif
+
+			Log.WriteLine("test");
+		}
+	}
+}
---
+++ a/Logging/Log.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+
+namespace PluralityUtilities.Logging
+{
+	public static class Log
+	{
+		private static readonly string _defaultLogFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/log/";
+		private static readonly string _defaultLogFileName = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".log";
+		private static string _logFolder = "";
+		private static string _logFileName = "";
+		private static string _logFilePath = "";
+
+
+		static Log()
+		{
+		}
+
+
+		public static void SetLogFileName(string filename)
+		{
+			_logFileName = filename;
+			SetLogFilePath();
+		}
+
+		public static void SetLogFolder(string folder)
+		{
+			_logFolder = folder;
+			var lastChar = folder[folder.Length - 1];
+			if (lastChar != '\\' && lastChar != '/')
+			{
+				_logFolder += '/';
+			}
+			Directory.CreateDirectory(_logFolder);
+			SetLogFilePath();
+		}
+
+		public static void Write(string message = "")
+		{
+			if (_logFolder == "")
+			{
+				SetLogFolder(_defaultLogFolder);
+			}
+			if (_logFileName == "")
+			{
+				SetLogFileName(_defaultLogFileName);
+			}
+			using (StreamWriter logFile = new StreamWriter(_logFilePath))
+			{
+				logFile.Write(DateTime.Now.ToString("yyyy-MM-dd:HH:mm:ss - ") + message);
+			}
+		}
+
+		public static void WriteLine(string message = "")
+		{
+			Write(message + '\n');
+		}
+
+
+		private static void SetLogFilePath()
+		{
+			_logFilePath = _logFolder + _logFileName;
+		}
+	}
+}
