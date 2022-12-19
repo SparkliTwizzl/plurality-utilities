affec45d945bb2d0bc7f5211d65a56294c0b3f12
SparkliTwizzl <github@sparklitwizzl.com>
2022-12-19 04:30:29 -0700

    restore actual files from before recreating solution and remove backups of them

--- PluralityUtilities/App/Program.cs
+++ PluralityUtilities/App/Program.cs
@@ -1,3 +1,16 @@
-﻿using Logging;
+﻿#if RELEASE
+using System.Diagnostics;
+using System.IO;
+#endif
+using System.Reflection;
 
-Log.WriteLine("Hello, World!");
+using PluralityUtilities.Logging;
+
+
+#if DEBUG
+Log.SetLogFolder(SolutionFolders.SolutionDir + "/run/log/");
+#elif RELEASE
+Log.SetLogFolder(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/" + Process.GetCurrentProcess().ProcessName + "_log/");
+#endif
+
+Log.WriteLine("execution started");
