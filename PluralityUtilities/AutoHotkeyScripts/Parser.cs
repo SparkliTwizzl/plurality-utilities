9b4d4b5f54ed5f1f1801085707fb81b9bfdfdbf9
SparkliTwizzl <github@sparklitwizzl.com>
2022-12-20 05:21:08 -0700

    add project for generating AutoHotkey scripts from formatted input files

---
+++ PluralityUtilities/AutoHotkeyScripts/Parser.cs
@@ -0,0 +1,14 @@
+﻿namespace PluralityUtilities.AutoHotkeyScripts
+{
+	public class Parser
+	{
+		public List<Person> People { get; private set; } = new List<Person>();
+
+
+		public void ParseFile(string filePath)
+		{
+			var inputData = File.ReadAllLines(filePath);
+
+		}
+	}
+}
---
+++ PluralityUtilities/AutoHotkeyScripts/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace PluralityUtilities.AutoHotkeyScripts
+{
+	public class Person
+	{
+		public List<string> Names { get; set; } = new List<string>();
+		public string Pronoun { get; set; } = "";
+	}
+}
