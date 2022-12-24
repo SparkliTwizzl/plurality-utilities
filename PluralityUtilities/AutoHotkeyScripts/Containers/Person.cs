1d6129e749f42c7be6fb520e25eda568131097d0
SparkliTwizzl <github@sparklitwizzl.com>
2022-12-24 01:54:27 -0700

    sort properties in Person class alphabetically

--- PluralityUtilities/AutoHotkeyScripts/Containers/Person.cs
+++ PluralityUtilities/AutoHotkeyScripts/Containers/Person.cs
@@ -3,7 +3,7 @@
 	public class Person
 	{
 		public List<string> Names { get; set; } = new List<string>();
-		public List<string> Tags { get; set; } = new List<string>();
 		public string Pronoun { get; set; } = string.Empty;
+		public List<string> Tags { get; set; } = new List<string>();
 	}
 }
