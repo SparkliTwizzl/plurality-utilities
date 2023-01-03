8d3c4a620549812f4b299b43498e06c0997de373
Athena | SparkliTwizzl <github@sparklitwizzl.com>
2023-01-03 15:35:24 -0700

    add test class for AutoHotkeyScriptGenerator

---
+++ PluralityUtilities/AutoHotkeyScriptsTests/Utilities/AutoHotkeyScriptGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PluralityUtilities.Logging;
+using PluralityUtilities.TestCommon;
+
+
+namespace PluralityUtilities.AutoHotkeyScripts.Utilities.Tests
+{
+	[TestClass]
+	public class AutoHotkeyScriptGeneratorTests
+	{
+		[TestInitialize]
+		public void Setup()
+		{
+			Log.SetLogFolder(TestDirectories.TestLogDir);
+			Log.SetLogFileName(DateTime.Now.ToString("test_yyyy-MM-dd_hh-mm-ss.log"));
+			Log.EnableVerbose();
+		}
+
+
+		[TestMethod]
+		public void GenerateScriptTest_Success()
+		{
+			Assert.Fail();
+		}
+	}
+}
