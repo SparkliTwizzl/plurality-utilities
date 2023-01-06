facb7ef7559475beda84e2fe9f518f1aadc727d7
Faith (they/them) | SparkliTwizzl <github@sparklitwizzl.com>
2023-01-06 09:52:41 -0700

    replace copied data in TestData/InputData by setting value to identical data in ExpectedOutputData

--- PluralityUtilities/AutoHotkeyScriptsTests/TestData/InputData.cs
+++ PluralityUtilities/AutoHotkeyScriptsTests/TestData/InputData.cs
@@ -13,38 +13,7 @@ namespace PluralityUtilities.AutoHotkeyScriptsTests.TestData
 
 		public static class TemplateParserData
 		{
-			public static readonly Person[] ValidPeople = new Person[]
-			{
-				new Person()
-				{
-					Identities = new List<Identity>()
-					{
-						new Identity()
-						{
-							Name = "Sam",
-							Tag = "sm",
-						},
-						new Identity()
-						{
-							Name = "Sammy",
-							Tag = "smy",
-						},
-					},
-					Pronoun = "they/them",
-					Decoration = "-> a person",
-				},
-				new Person()
-				{
-					Identities = new List<Identity>()
-					{
-						new Identity()
-						{
-							Name = "Alex",
-							Tag = "ax",
-						},
-					},
-				}
-			};
+			public static readonly Person[] ValidPeople = ExpectedOutputData.ParsedInputData;
 			public static readonly string[] ValidTemplates = ExpectedOutputData.ParsedTemplateData;
 		}
 }
