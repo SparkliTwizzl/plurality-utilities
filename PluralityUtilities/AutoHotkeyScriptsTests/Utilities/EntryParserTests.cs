023809a310e1a3a2654ce45293600f548f037dbe
Whimsy Wolfe (she/her) | SparkliTwizzl <github@sparklitwizzl.com>
2023-06-02 00:39:37 -0600

    add test class for EntryParser

---
+++ PluralityUtilities/AutoHotkeyScriptsTests/Utilities/EntryParserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluralityUtilities.TestCommon.Utilities;
+
+namespace PluralityUtilities.AutoHotkeyScripts.Utilities.Tests
+{
+	[ TestClass ]
+	public class EntryParserTests
+	{
+		[TestInitialize]
+		public void Setup()
+		{
+			TestUtilities.InitializeLoggingForTests();
+		}
+
+
+		[ TestMethod ]
+		public void ParseEntriesFromDataTest_()
+		{
+			Assert.Fail();
+		}
+	}
+}
