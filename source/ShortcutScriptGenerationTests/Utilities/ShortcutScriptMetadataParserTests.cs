f461633bad947cb24642e59f6eb3a5ea058ae367
Xandra Collins (she/her) | SparkliTwizzl <github@sparklitwizzl.com>
2023-12-19 23:12:34 -0700

    add case to metadata parser tests for mismatched brackets

--- source/ShortcutScriptGenerationTests/Utilities/ShortcutScriptMetadataParserTests.cs
+++ source/ShortcutScriptGenerationTests/Utilities/ShortcutScriptMetadataParserTests.cs
@@ -14,6 +14,14 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities.Tests
 			public static string IconPath => "path/to/icon.ico";
 			public static ShortcutScriptMetadata MetadataWithOptionalData => new( IconPath );
 			public static ShortcutScriptMetadata MetadataWithoutOptionalData => new();
+			public static string[] RegionDataWithDanglingCloseBracket => new[]
+			{
+				"}",
+			};
+			public static string[] RegionDataWithDanglingOpenBracket => new[]
+			{
+				"{",
+			};
 			public static string[] RegionDataWithUnknownToken => new[]
 			{
 				"{",
@@ -68,6 +76,23 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities.Tests
 		public void ParseMetadataFromDataTest_ThrowsUnknownTokenException()
 		{
 			_ = metadataParser!.ParseMetadataFromData(TestData.RegionDataWithUnknownToken, ref i);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof( BracketMismatchException ))]
+		[DynamicData(nameof(ParseMetadataFromDataTest_ThrowsBracketMismatchException_Data), DynamicDataSourceType.Property)]
+		public void ParseMetadataFromDataTest_ThrowsBracketMismatchException(string[] regionData)
+		{
+			_ = metadataParser!.ParseMetadataFromData(regionData, ref i);
+		}
+
+		public static IEnumerable<object[]> ParseMetadataFromDataTest_ThrowsBracketMismatchException_Data
+		{
+			get
+			{
+				yield return new object[] { TestData.RegionDataWithDanglingCloseBracket };
+				yield return new object[] { TestData.RegionDataWithDanglingOpenBracket };
+			}
 		}
 	}
 }
