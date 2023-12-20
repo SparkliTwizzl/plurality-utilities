ccde3a0f67b5272f48b4262dbec85bd9db0aa305
Xandra Collins (she/her) | SparkliTwizzl <github@sparklitwizzl.com>
2023-12-19 23:13:19 -0700

    add implementation of metadata parser behavior*
    *known bug with parsing tokens, working on it

--- source/ShortcutScriptGeneration/Utilities/ShortcutScriptMetadataParser.cs
+++ source/ShortcutScriptGeneration/Utilities/ShortcutScriptMetadataParser.cs
@@ -1,4 +1,8 @@
-﻿using Petrichor.ShortcutScriptGeneration.Containers;
+﻿using Petrichor.Common.Containers;
+using Petrichor.Common.Enums;
+using Petrichor.Logging;
+using Petrichor.ShortcutScriptGeneration.Containers;
+using Petrichor.ShortcutScriptGeneration.Exceptions;
 
 
 namespace Petrichor.ShortcutScriptGeneration.Utilities
@@ -6,12 +10,85 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 	public class ShortcutScriptMetadataParser : IShortcutScriptMetadataParser
 	{
 		private const string DefaultIconFilePathToken = "default-icon:";
-		private StringTokenParser TokenParser = new StringTokenParser();
+
+		private int IndentLevel { get; set; } = 0;
+		private ShortcutScriptMetadata Metadata { get; set; } = new();
 
 
-		public ShortcutScriptMetadata ParseMetadataFromData(string[] data, ref int i)
+		public ShortcutScriptMetadata ParseMetadataFromData( string[] data, ref int i )
 		{
-			throw new NotImplementedException();
+			var taskMessage = "parsing metadata region data";
+			Log.TaskStarted( taskMessage );
+
+			var expectedTokens = new string[]
+			{
+				DefaultIconFilePathToken,
+			};
+			for (; i < data.Length; ++i)
+			{
+				var token = new StringToken( data[ i ] );
+				var errorMessage = string.Empty;
+				var isParsingFinished = false;
+				switch ( token.Name )
+				{
+					case "{":
+						{
+							++IndentLevel;
+							break;
+						}
+
+					case "}":
+						{
+							--IndentLevel;
+
+							if ( IndentLevel < 0 )
+							{
+								errorMessage = $"a mismatched closing curly brace was found when parsing metadata region";
+								Log.Error( errorMessage );
+								throw new BracketMismatchException( errorMessage );
+							}
+
+							if ( IndentLevel == 0 )
+							{
+								isParsingFinished = true;
+							}
+							break;
+						}
+
+					case DefaultIconFilePathToken:
+						{
+							StoreDefaultIconPath( token.Value );
+							break;
+						}
+
+					default:
+						{
+							errorMessage = $"an unrecognized token ({ token.Value }) was found when parsing metadata region";
+							Log.Error( errorMessage );
+							throw new UnknownTokenException( errorMessage );
+						}
+				}
+				if ( isParsingFinished )
+				{
+					break;
+				}
+			}
+
+			if ( IndentLevel != 0 )
+			{
+				var errorMessage = $"a mismatched curly brace was found when parsing metadata region";
+				Log.Error( errorMessage );
+				throw new BracketMismatchException( errorMessage );
+			}
+
+			Log.TaskFinished( taskMessage );
+			return Metadata;
+		}
+
+
+		private void StoreDefaultIconPath( string filePath )
+		{
+			Metadata.DefaultIconPath = filePath;
 		}
 	}
 }
