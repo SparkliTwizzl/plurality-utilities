20b2d0ebedc963bd0dc42eb68eb351401a3c5256
Xandra Collins (she/her) | SparkliTwizzl <github@sparklitwizzl.com>
2023-12-19 22:28:21 -0700

    add new data container for string tokens to automatically split name and value from raw token

---
+++ source/Common/Containers/StringToken.cs
@@ -0,0 +1,38 @@
+﻿namespace Petrichor.Common.Containers
+{
+	public class StringToken
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Value { get; set; } = string.Empty;
+
+
+		public StringToken() { }
+
+		public StringToken( StringToken other )
+		{
+			Name = other.Name;
+			Value = other.Value;
+		}
+
+		public StringToken( string rawToken )
+		{
+			SplitAndStoreRawToken( rawToken );
+		}
+
+		public StringToken( QualifiedStringToken qualifiedToken )
+		{
+			SplitAndStoreRawToken( qualifiedToken.Value );
+		}
+
+
+		private void SplitAndStoreRawToken( string rawToken )
+		{
+			var nameEndsAt = rawToken.IndexOf( ':' );
+			var valueStartsAt = nameEndsAt + 1;
+			var valueLength = rawToken.Length - valueStartsAt;
+
+			Name = rawToken.Substring( 0, nameEndsAt ).Trim();
+			Value = rawToken.Substring( valueStartsAt, valueLength );
+		}
+	}
+}
