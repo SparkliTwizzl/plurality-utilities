9cfce34c2818f04ecaa65901a9419f2f309f7355
Jack Halloway (he/him) | SparkliTwizzl <github@sparklitwizzl.com>
2023-12-24 00:56:44 -0700

    implement parsing and writing suspend shortcut to output script, account for no shortcuts being present, update unit test data
    it occurred to me that the reload and suspend shortcuts should not be written to the output file if not provided or the script would break.
    also i forgot some stuff in the test data and had to fix that.

--- source/ShortcutScriptGeneration/Utilities/ShortcutScriptGenerator.cs
+++ source/ShortcutScriptGeneration/Utilities/ShortcutScriptGenerator.cs
@@ -90,6 +90,28 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 			};
 			WriteLinesToFile(lines);
 		}
+
+		private void WriteControlShortcutToFile()
+		{
+			if ( Input.Metadata.ReloadShortcut == string.Empty && Input.Metadata.SuspendShortcut == string.Empty )
+			{
+				return;
+			}
+
+			WriteLineToFile( "; script reload / suspend shortcut(s)" );
+			if ( Input.Metadata.ReloadShortcut != string.Empty )
+			{
+				WriteLineToFile( $"{ Input.Metadata.ReloadShortcut }::Reload()" );
+			}
+			if ( Input.Metadata.SuspendShortcut != string.Empty )
+			{
+				WriteLineToFile( "#SuspendExempt true" );
+				WriteLineToFile( $"{ Input.Metadata.SuspendShortcut }::Suspend( SUSPEND_TOGGLE )" );
+				WriteLineToFile( "#SuspendExempt false" );
+			}
+			WriteLineToFile();
+			WriteLineToFile();
+		}
 		
 		private void WriteControlStatementsToFile()
 		{
@@ -124,7 +146,7 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 			WriteIconFilePathsToFile();
 			WriteConstantsToFile();
 			WriteIconHandlingToFile();
-			WriteReloadShortcutToFile();
+			WriteControlShortcutToFile();
 			Log.TaskFinished(taskMessage);
 		}
 
@@ -229,18 +251,6 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 			WriteLineToFile( "; macros generated from entries and templates" );
 			WriteLinesToFile(Input.Macros);
 			Log.TaskFinished(taskMessage);
-		}
-
-		private void WriteReloadShortcutToFile()
-		{
-			var lines = new string[]
-			{
-				"; script reload shortcut",
-				$"{ Input.Metadata.ReloadShortcut }::Reload()",
-				"",
-				"",
-			};
-			WriteLinesToFile(lines);
 		}
 	}
 }
