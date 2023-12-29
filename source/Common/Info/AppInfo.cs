aeec28838de29bd6f67ea72512c302cc9e7199ff
[Roxanne Witchcraft (roi/rois)] SparkliTwizzl <github@sparklitwizzl.com>
2023-12-29 03:34:51 -0700

    split version string into major/minor/patch, make everything in AppInfo struct into public properties

--- source/Common/Info/AppInfo.cs
+++ source/Common/Info/AppInfo.cs
@@ -2,17 +2,19 @@ namespace Petrichor.Common.Info
 {
 	public struct AppInfo
 	{
-		private const string releaseAppVersion = "0.9";
-		private const string developmentAppVersion = $"{releaseAppVersion}-dev";
-
-
-		public const string AppName = "Petrichor";
+		public static string AppName => "Petrichor";
 		public static string AppNameAndVersion => $"{AppName} v{AppVersion}";
 		public static string AppVersion =>
 #if DEBUG
-			developmentAppVersion;
+			DevelopmentAppVersion;
 #else
-			releaseAppVersion;
+			ReleaseAppVersion;
 #endif
+		public static string DevelopmentAppVersion => $"{ReleaseAppVersion}-dev";
+		public static string MajorVersion => "0";
+		public static string MinorVersion => "9";
+		public static string PatchVersion => "0";
+		public static string PreReleaseVersion => "";
+		public static string ReleaseAppVersion => $"{MajorVersion}.{MinorVersion}.{PatchVersion}{PreReleaseVersion}";
 	}
 }
