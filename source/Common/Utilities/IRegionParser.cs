188c999d81261c4f37ab0afac29c6f947ab4ea06
[Vilense Queen (she/her)] SparkliTwizzl <github@sparklitwizzl.com>
2023-12-30 03:44:28 -0700

    add region parser interface to Common

---
+++ source/Common/Utilities/IRegionParser.cs
@@ -0,0 +1,12 @@
+﻿namespace Petrichor.Common.Utilities
+{
+	public interface IRegionParser
+	{
+		bool HasParsedMaxAllowedRegions { get; }
+		int MaxRegionsAllowed { get; }
+		int RegionsParsed {  get; }
+
+		
+		T Parse<T>( string[] regionData, ref int i );
+	}
+}
