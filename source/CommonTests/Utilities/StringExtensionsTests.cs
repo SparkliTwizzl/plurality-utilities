e357edf85a82ede8e271158ed2f859c11744697a
[Amanda Holiday (she/her)] SparkliTwizzl <github@sparklitwizzl.com>
2023-12-30 09:47:30 -0700

    add unit tests for string extensions class

---
+++ source/CommonTests/Utilities/StringExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Petrichor.Common.Utilities.Tests
+{
+	[TestClass]
+	public class StringExtensionsTests
+	{
+		public struct TestData
+		{
+			public const string WrapInQuotes_Expected = "\"text\"";
+		}
+
+		[TestMethod]
+		[DataRow( "text" )]
+		[DataRow( "\"text" )]
+		[DataRow( "text\"" )]
+		[DataRow( "\"text\"" )]
+		public void WrapInQuotes_Test_Success( string input )
+		{
+			var expected = TestData.WrapInQuotes_Expected;
+			var actual = input.WrapInQuotes();
+			Assert.AreEqual( expected, actual );
+		}
+	}
+}
