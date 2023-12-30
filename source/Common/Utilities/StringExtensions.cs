a9fb62c0ad48bdd3246c38c0f87bb5082e68d818
[Vilense Queen (she/her)] SparkliTwizzl <github@sparklitwizzl.com>
2023-12-30 08:45:59 -0700

    add string extension class to wrap strings in quotes if they are not already

---
+++ source/Common/Utilities/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Petrichor.Common.Utilities
+{
+	public static class StringExtensions
+	{
+		public static string WrapInQuotes( this string input )
+		{
+			var startsWithQuote = input.StartsWith( '"' );
+			var endsWithQuote = input.EndsWith( '"' );
+			if ( !startsWithQuote && !endsWithQuote )
+			{
+				return string.Format( "\"{0}\"", input );
+			}
+			if ( !startsWithQuote )
+			{
+				return string.Format( "\"{0}", input );
+			}
+			if ( !endsWithQuote )
+			{
+				return string.Format( "{0}\"", input );
+			}
+			return input;
+		}
+	}
+}
