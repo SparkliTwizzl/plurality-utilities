c49b102bc3f958bed49f3fee95d00189add78ad3
[Nocturne (mirror pronouns)] SparkliTwizzl <github@sparklitwizzl.com>
2024-01-05 11:58:37 -0700

    fix incorrect replace value for [tab], remove newline sequences
    the tab sequence was not value AutoHotkey syntax, and the newline sequences serve no purpose in this instance.

--- source/ShortcutScriptGenerationTests/Utilities/ModuleOptionsRegionParserTests.cs
+++ source/ShortcutScriptGenerationTests/Utilities/ModuleOptionsRegionParserTests.cs
@@ -58,9 +58,9 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities.Tests
 			};
 			public static string ReloadShortcut => "reloadshortcut";
 			public static string Shortcut_AllFindStrings
-				=> "[windows] [win] [alt] [left-alt] [lalt] [right-alt] [ralt] [control] [ctrl] [left-control] [lctrl] [right-control] [rctrl] [shift] [left-shift] [lshift] [right-shift] [rshift] [and] [alt-graph] [altgr] [wildcard] [wild] [passthrough] [tilde] [send] [newline] [nl] [tab] [caps-lock] [caps] [enter] [backspace] [bksp] [insert] [ins] [delete] [del] [home] [end] [page-up] [pgup] [page-down] [pgdn] [unknown] \\[del\\]";
+				=> "[windows] [win] [alt] [left-alt] [lalt] [right-alt] [ralt] [control] [ctrl] [left-control] [lctrl] [right-control] [rctrl] [shift] [left-shift] [lshift] [right-shift] [rshift] [and] [alt-graph] [altgr] [wildcard] [wild] [passthrough] [tilde] [send] [tab] [caps-lock] [caps] [enter] [backspace] [bksp] [insert] [ins] [delete] [del] [home] [end] [page-up] [pgup] [page-down] [pgdn] [unknown] \\[del\\]";
 			public static string Shortcut_AllReplaceStrings
-				=> "# # ! <! <! >@ >@ ^ ^ <^ <^ >^ >^ + <+ <+ >+ >+ & <^>! <^>! * * ~ ~ $ \n` \n` `` CapsLock CapsLock Enter Backspace Backspace Insert Insert Delete Delete Home End PageUp PageUp PageDown PageDown [unknown] [del]";
+				=> "# # ! <! <! >@ >@ ^ ^ <^ <^ >^ >^ + <+ <+ >+ >+ & <^>! <^>! * * ~ ~ $ Tab CapsLock CapsLock Enter Backspace Backspace Insert Insert Delete Delete Home End PageUp PageUp PageDown PageDown [unknown] [del]";
 			public static string SuspendIconPath => "path/to/suspendicon.ico";
 			public static string SuspendIconPathWithQuotes => $"\"{SuspendIconPath}\"";
 			public static string SuspendShortcut => "suspendshortcut";
