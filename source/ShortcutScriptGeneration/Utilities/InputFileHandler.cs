4a211d863675f3fe81a523f34f4e7340ba63e678
[Mikki Hollis (they/them)] SparkliTwizzl <github@sparklitwizzl.com>
2024-03-16 04:25:54 -0600

    extract fields and methods in input file handler class

--- source/ShortcutScriptGeneration/Utilities/InputFileHandler.cs
+++ source/ShortcutScriptGeneration/Utilities/InputFileHandler.cs
@@ -10,11 +10,72 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 	public class InputFileHandler
 	{
 		private IEntriesRegionParser EntriesRegionParser { get; set; }
+		private IRegionParser< ScriptInput > FileRegionParser { get; set; }
 		private IMacroGenerator MacroGenerator { get; set; }
 		private IRegionParser< StringWrapper > MetadataRegionParser { get; set; }
 		private IModuleOptionsRegionParser ModuleOptionsRegionParser { get; set; }
 		private ITemplatesRegionParser TemplatesRegionParser { get; set; }
-		private IRegionParser< ScriptInput > FileRegionParser { get; set; }
+
+
+		private Func< string[], int, ScriptInput, RegionData< ScriptInput > > EntriesTokenHandler =>
+			( string[] fileData, int regionStartIndex, ScriptInput result ) =>
+				{
+					var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
+					result.Entries = EntriesRegionParser.Parse( dataTrimmedToRegion );
+					return new RegionData< ScriptInput >()
+					{
+						BodySize = EntriesRegionParser.LinesParsed,
+						Value = result,
+					};
+				};
+
+		private Func< string[], int, ScriptInput, RegionData< ScriptInput > > MetadataTokenHandler =>
+			( string[] fileData, int regionStartIndex, ScriptInput result ) =>
+				{
+					var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
+					var resultMessage = MetadataRegionParser.Parse( dataTrimmedToRegion ) ;
+					Log.Important( resultMessage.ToString() );
+					return new RegionData< ScriptInput >()
+					{
+						BodySize = MetadataRegionParser.LinesParsed,
+						Value = result,
+					};
+				};
+
+		private Func< string[], int, ScriptInput, RegionData< ScriptInput > > ModuleOptionsTokenHandler =>
+			( string[] fileData, int regionStartIndex, ScriptInput result ) =>
+				{
+					var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
+					result.ModuleOptions = ModuleOptionsRegionParser.Parse( dataTrimmedToRegion );
+					return new RegionData< ScriptInput >()
+					{
+						BodySize = ModuleOptionsRegionParser.LinesParsed,
+						Value = result,
+					};
+				};
+
+		private Func< ScriptInput, ScriptInput > PostParseHandler => ( ScriptInput result ) =>
+		{
+			if ( !MetadataRegionParser.HasParsedMinRequiredRegions )
+			{
+				ExceptionLogger.LogAndThrow( new FileRegionException( $"Input files must contain at least { MetadataRegionParser.MinRegionsRequired } { MetadataRegionParser.RegionName } regions" ) );
+			}
+
+			result.Macros = MacroGenerator.Generate( result );
+			return result;
+		};
+
+		private Func< string[], int, ScriptInput, RegionData< ScriptInput > > TemplatesTokenHandler =>
+			( string[] fileData, int regionStartIndex, ScriptInput result ) =>
+				{
+					var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
+					result.Templates = TemplatesRegionParser.Parse( dataTrimmedToRegion );
+					return new RegionData< ScriptInput >()
+					{
+						BodySize = TemplatesRegionParser.LinesParsed,
+						Value = result,
+					};
+				};
 
 
 		public InputFileHandler( IRegionParser< StringWrapper > metadataRegionParser, IModuleOptionsRegionParser moduleOptionsRegionParser, IEntriesRegionParser entriesRegionParser, ITemplatesRegionParser templatesRegionParser, IMacroGenerator macroGenerator )
@@ -24,80 +85,7 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 			MetadataRegionParser = metadataRegionParser;
 			ModuleOptionsRegionParser = moduleOptionsRegionParser;
 			TemplatesRegionParser = templatesRegionParser;
-
-			var entriesTokenHandler = ( string[] fileData, int regionStartIndex, ScriptInput result ) =>
-			{
-				var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
-				result.Entries = EntriesRegionParser.Parse( dataTrimmedToRegion );
-				return new RegionData< ScriptInput >()
-				{
-					BodySize = EntriesRegionParser.LinesParsed,
-					Value = result,
-				};
-			};
-
-			var metadataTokenHandler = ( string[] fileData, int regionStartIndex, ScriptInput result ) =>
-			{
-				var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
-				var resultMessage = MetadataRegionParser.Parse( dataTrimmedToRegion ) ;
-				Log.Important( resultMessage.ToString() );
-				return new RegionData< ScriptInput >()
-				{
-					BodySize = MetadataRegionParser.LinesParsed,
-					Value = result,
-				};
-			};
-
-			var moduleOptionsTokenHandler = ( string[] fileData, int regionStartIndex, ScriptInput result ) =>
-			{
-				var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
-				result.ModuleOptions = ModuleOptionsRegionParser.Parse( dataTrimmedToRegion );
-				return new RegionData< ScriptInput >()
-				{
-					BodySize = ModuleOptionsRegionParser.LinesParsed,
-					Value = result,
-				};
-			};
-
-			var templatesTokenHandler = ( string[] fileData, int regionStartIndex, ScriptInput result ) =>
-			{
-				var dataTrimmedToRegion = fileData[ ( regionStartIndex + 1 ).. ];
-				result.Templates = TemplatesRegionParser.Parse( dataTrimmedToRegion );
-				return new RegionData< ScriptInput >()
-				{
-					BodySize = TemplatesRegionParser.LinesParsed,
-					Value = result,
-				};
-			};
-
-			var tokenHandlers = new Dictionary< string, Func< string[], int, ScriptInput, RegionData< ScriptInput > > >()
-			{
-				{ Syntax.TokenNames.EntriesRegion, entriesTokenHandler },
-				{ Common.Syntax.TokenNames.MetadataRegion, metadataTokenHandler },
-				{ Syntax.TokenNames.ModuleOptionsRegion, moduleOptionsTokenHandler },
-				{ Syntax.TokenNames.TemplatesRegion, templatesTokenHandler },
-			};
-
-			var postParseHandler = ( ScriptInput result ) =>
-			{
-				if ( !MetadataRegionParser.HasParsedMinRequiredRegions )
-				{
-					ExceptionLogger.LogAndThrow( new FileRegionException( $"Input files must contain at least { MetadataRegionParser.MinRegionsRequired } { MetadataRegionParser.RegionName } regions" ) );
-				}
-
-				result.Macros = MacroGenerator.Generate( result );
-				return result;
-			};
-
-			var parserDescriptor = new RegionParserDescriptor< ScriptInput >()
-			{
-				RegionName = "Input file body",
-				MaxRegionsAllowed = 1,
-				MinRegionsRequired = 1,
-				TokenHandlers = tokenHandlers,
-				PostParseHandler = postParseHandler,
-			};
-			FileRegionParser = new RegionParser< ScriptInput >( parserDescriptor );
+			FileRegionParser = CreateRegionParser();
 		}
 
 
@@ -111,6 +99,29 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 
 			Log.TaskFinish( taskMessage );
 			return result;
+		}
+
+
+		private RegionParser< ScriptInput > CreateRegionParser()
+		{
+			var tokenHandlers = new Dictionary< string, Func< string[], int, ScriptInput, RegionData< ScriptInput > > >()
+			{
+				{ Syntax.TokenNames.EntriesRegion, EntriesTokenHandler },
+				{ Common.Syntax.TokenNames.MetadataRegion, MetadataTokenHandler },
+				{ Syntax.TokenNames.ModuleOptionsRegion, ModuleOptionsTokenHandler },
+				{ Syntax.TokenNames.TemplatesRegion, TemplatesTokenHandler },
+			};
+
+			var parserDescriptor = new RegionParserDescriptor< ScriptInput >()
+			{
+				RegionName = "Input file body",
+				MaxRegionsAllowed = 1,
+				MinRegionsRequired = 1,
+				TokenHandlers = tokenHandlers,
+				PostParseHandler = PostParseHandler,
+			};
+
+			return new RegionParser< ScriptInput >( parserDescriptor );
 		}
 	}
 }
