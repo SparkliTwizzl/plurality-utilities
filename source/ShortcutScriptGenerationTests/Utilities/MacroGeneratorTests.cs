3b848ece78c2dd974918781888c0c406cc170349
[Xandra Collins (she/her)] SparkliTwizzl <github@sparklitwizzl.com>
2024-03-27 15:55:33 -0600

    update macro generator behavior and tests

--- source/ShortcutScriptGenerationTests/Utilities/MacroGeneratorTests.cs
+++ source/ShortcutScriptGenerationTests/Utilities/MacroGeneratorTests.cs
@@ -12,7 +12,7 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities.Tests
 	{
 		public readonly struct TestData
 		{
-			public static ScriptEntry[] Entries => new[]
+			public static ScriptEntry[] EntryList => new[]
 			{
 				new ScriptEntry(
 					EntryID,
@@ -31,17 +31,31 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities.Tests
 			public static string EntryLastTag => "LAST_TAG";
 			public static string EntryPronoun => "PRONOUN";
 			public static string EntryTag => "TAG";
-			public static ScriptInput Input => new( ModuleOptions, Entries, Templates );
+			public static ScriptInput Input => new( ModuleOptions, EntryList, TemplateList );
 			public static string[] Macros => new[]
 			{
-				$"::{ Common.Syntax.ControlSequences.Escape }{ EntryTag }{ EntryLastTag }:: { Common.Syntax.ControlSequences.FindTagOpen }{ EntryID }{ Common.Syntax.ControlSequences.FindTagClose } { EntryName } { EntryLastName } { EntryPronoun } { EntryColor } { EntryDecoration } `",
+				$"::{ Common.Syntax.ControlSequences.Escape }{ EntryTag }{ EntryLastTag }:: { Common.Syntax.ControlSequences.FindTagOpen }{ EntryID }{ Common.Syntax.ControlSequences.FindTagClose } { EntryName } { EntryLastName } { EntryPronoun } { EntryColor } { EntryDecoration } {TemplateReplace1}{TemplateReplace2} `",
 			};
 			public static ScriptModuleOptions ModuleOptions => new( TestAssets.DefaultIconFileName, TestAssets.SuspendIconFilePath, TestAssets.ReloadShortcut, TestAssets.SuspendShortcut );
-			public static string Template => $"::{Common.Syntax.ControlSequences.EscapeStandin}{TemplateFindStrings.Tag}{TemplateFindStrings.LastTag}:: {Common.Syntax.ControlSequences.FindTagOpenStandin}{TemplateFindStrings.ID}{Common.Syntax.ControlSequences.FindTagCloseStandin} {TemplateFindStrings.Name} {TemplateFindStrings.LastName} {TemplateFindStrings.Pronoun} {TemplateFindStrings.Color} {TemplateFindStrings.Decoration} `";
-			public static string[] Templates => new[]
+			public static ScriptMacroTemplate Template => new()
+			{
+				TemplateString = TemplateString,
+				FindAndReplace = TemplateFindAndReplace,
+			};
+			public static string TemplateFind1 => "find1";
+			public static string TemplateFind2 => "find2";
+			public static Dictionary<string, string> TemplateFindAndReplace => new()
+			{
+				{ TemplateFind1, TemplateReplace1 },
+				{ TemplateFind2, TemplateReplace2 },
+			};
+			public static ScriptMacroTemplate[] TemplateList => new[]
 			{
 				Template,
 			};
+			public static string TemplateReplace1 => "replace1";
+			public static string TemplateReplace2 => "replace2";
+			public static string TemplateString => $"::{Common.Syntax.ControlSequences.EscapeStandin}{TemplateFindStrings.Tag}{TemplateFindStrings.LastTag}:: {Common.Syntax.ControlSequences.FindTagOpenStandin}{TemplateFindStrings.ID}{Common.Syntax.ControlSequences.FindTagCloseStandin} {TemplateFindStrings.Name} {TemplateFindStrings.LastName} {TemplateFindStrings.Pronoun} {TemplateFindStrings.Color} {TemplateFindStrings.Decoration} {TemplateFind1}{TemplateFind2} `";
 		}
 
 
