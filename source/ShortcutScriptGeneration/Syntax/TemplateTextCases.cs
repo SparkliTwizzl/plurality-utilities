7094d9e7bc4dbeb133d867ff93440a81a5210360
[Eleanor Butler (neo'sthey)] SparkliTwizzl <github@sparklitwizzl.com>
2024-03-28 15:52:16 -0600

    add data struct for text cases

---
+++ source/ShortcutScriptGeneration/Syntax/TemplateTextCases.cs
@@ -0,0 +1,20 @@
+﻿namespace Petrichor.ShortcutScriptGeneration.Syntax
+{
+	public readonly struct TemplateTextCases
+	{
+		public const string Default = Unchanged;
+		public const string FirstCaps = "firstCaps";
+		public const string Lower = "lower";
+		public const string Unchanged = "unchanged";
+		public const string Upper = "upper";
+
+		public static string[] LookUpTable => new[]
+		{
+			Default,
+			FirstCaps,
+			Lower,
+			Unchanged,
+			Upper,
+		};
+	}
+}
