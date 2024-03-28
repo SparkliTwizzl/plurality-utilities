566da3bffcf42338512e5b6f5e4a00188d91400b
[Faith Priest (they/them)] SparkliTwizzl <github@sparklitwizzl.com>
2024-03-28 03:00:12 -0600

    move helper methods into helper classes for organization, get required exceptions implemented for new template behavior

--- source/ShortcutScriptGeneration/Utilities/TemplateHandler.cs
+++ source/ShortcutScriptGeneration/Utilities/TemplateHandler.cs
@@ -11,6 +11,174 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 {
 	public static class TemplateHandler
 	{
+		private static class FindAndReplaceParseHandler
+		{
+			public static Dictionary<string, string> ParseFindKeys( StringToken token )
+			{
+				var result = new Dictionary<string, string>();
+				var items = ExtractItemsFromBody( token );
+				foreach ( var item in items )
+				{
+					result.Add( item, string.Empty );
+				}
+				return result;
+			}
+
+			public static Dictionary<string, string> ParseReplaceValues( StringToken token, Dictionary<string, string> result )
+			{
+				var items = ExtractItemsFromBody( token );
+				if ( items.Length != result.Count )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"A(n) \"{token.Key}\" token's body has a different number of items than its corresponding \"{Tokens.Find.Key}\" token (has: {items.Length} / should have: {result.Count})." ), token.LineNumber );
+				}
+				for ( var i = 0 ; i < items.Length ; ++i )
+				{
+					var key = result.ElementAt( i ).Key;
+					result[ key ] = items[ i ];
+				}
+				return result;
+			}
+
+
+			private static string[] ExtractItemsFromBody( StringToken token )
+			{
+				if ( token.Value == string.Empty )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"\"{token.Key}\" token values cannot be blank." ), token.LineNumber );
+				}
+				if ( !token.Value.StartsWith( Common.Syntax.ControlSequences.RegionOpen ) )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"\"{token.Key}\" token bodies must start with a '{Common.Syntax.ControlSequences.RegionOpen}' character." ), token.LineNumber );
+				}
+				if ( !token.Value.EndsWith( Common.Syntax.ControlSequences.RegionClose ) )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"\"{token.Key}\" token bodies must end with a '{Common.Syntax.ControlSequences.RegionClose}' character." ), token.LineNumber );
+				}
+				var items = token.Value[ 1..( token.Value.Length - 1 ) ];
+				return items.Split( ',' );
+			}
+		}
+
+
+		private static class TemplateParseHandler
+		{
+			public static string ParseTemplateString( StringToken token )
+			{
+				var rawHotstring = ConvertTemplateToAutoHotkeySyntax( token.Value, token.LineNumber );
+				var sanitizedHotstring = SanitizeHotstring( rawHotstring );
+
+				var template = new StringBuilder();
+				for ( var i = 0 ; i < sanitizedHotstring.Length ; ++i )
+				{
+					var c = sanitizedHotstring[ i ];
+
+					if ( c == Common.Syntax.ControlSequences.FindTagClose )
+					{
+						ExceptionLogger.LogAndThrow( new TokenValueException( $"A template contained a mismatched find-string close character ('{Common.Syntax.ControlSequences.FindTagClose}')." ), token.LineNumber );
+						break;
+					}
+
+					else if ( c == Common.Syntax.ControlSequences.FindTagOpen )
+					{
+						var substring = sanitizedHotstring[ i.. ];
+						var findString = ExtractFindTagFromLine( substring, token.LineNumber );
+						ValidateFindTagValue( findString, token.LineNumber );
+						_ = template.Append( findString );
+						i += findString.Length - 1;
+					}
+
+					else if ( c == Common.Syntax.ControlSequences.Escape )
+					{
+						try
+						{
+							_ = template.Append( sanitizedHotstring[ i..( i + 2 ) ] );
+							++i;
+							continue;
+						}
+						catch ( Exception exception )
+						{
+							ExceptionLogger.LogAndThrow( new TokenValueException( $"A template contained a dangling escape character ('{Common.Syntax.ControlSequences.Escape}') with no following character to escape.", exception ), token.LineNumber );
+							break;
+						}
+					}
+
+					else
+					{
+						_ = template.Append( c );
+					}
+				}
+				return template.ToString();
+			}
+
+
+			private static string ConvertTemplateToAutoHotkeySyntax( string template, int lineNumber )
+			{
+				var components = template.Split( ControlSequences.TemplateFindReplaceDivider );
+				var doesFindStringExist = components[ 0 ]?.Length > 0;
+				var doesReplaceStringExist = components[ 1 ]?.Length > 0;
+				var isTemplateInValidFormat = doesFindStringExist && doesReplaceStringExist;
+				if ( !isTemplateInValidFormat )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"A(n) \"{Tokens.Template.Key}\" token's value is not a valid template string." ), lineNumber );
+				}
+
+				var findString = components[ 0 ].Trim();
+				var replaceString = components[ 1 ].Trim();
+				return $"::{findString}::{replaceString}";
+			}
+
+			private static string ExtractFindTagFromLine( string line, int lineNumber )
+			{
+				var lengthOfFindString = GetLengthOfFindTag( line, lineNumber );
+				return line[ ..lengthOfFindString ];
+			}
+
+			private static int GetIndexOfNextFindTagCloseChar( string line, int lineNumber )
+			{
+				var nextCloseCharIndex = line.IndexOf( Common.Syntax.ControlSequences.FindTagClose );
+				var nextOpenCharIndex = line.IndexOf( Common.Syntax.ControlSequences.FindTagOpen );
+
+				var hasNoCloseChar = nextCloseCharIndex < 0;
+				var hasMismatchedOpenChar = nextOpenCharIndex >= 0 && nextCloseCharIndex > nextOpenCharIndex;
+				if ( hasNoCloseChar || hasMismatchedOpenChar )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"A template contained a mismatched \"find\" tag open character ( '{Common.Syntax.ControlSequences.FindTagOpen}' )." ), lineNumber );
+				}
+
+				var isCloseCharEscaped = line[ nextCloseCharIndex - 1 ] == Common.Syntax.ControlSequences.Escape;
+				if ( isCloseCharEscaped )
+				{
+					var substring = line[ ( nextCloseCharIndex + 1 ).. ];
+					return nextCloseCharIndex + GetIndexOfNextFindTagCloseChar( substring, lineNumber );
+				}
+
+				return nextCloseCharIndex;
+			}
+
+			private static int GetLengthOfFindTag( string findTag, int lineNumber )
+			{
+				var findTagOpenCharTrimmed = findTag[ 1.. ];
+				var findTagValueLength = GetIndexOfNextFindTagCloseChar( findTagOpenCharTrimmed, lineNumber );
+				return findTagValueLength + 2;
+			}
+
+			private static string SanitizeHotstring( string rawHotstring )
+				=> rawHotstring
+					.Replace( $"{Common.Syntax.ControlSequences.Escape}{Common.Syntax.ControlSequences.Escape}", Common.Syntax.ControlSequences.EscapeStandin )
+					.Replace( $"{Common.Syntax.ControlSequences.Escape}{Common.Syntax.ControlSequences.FindTagOpen}", Common.Syntax.ControlSequences.FindTagOpenStandin )
+					.Replace( $"{Common.Syntax.ControlSequences.Escape}{Common.Syntax.ControlSequences.FindTagClose}", Common.Syntax.ControlSequences.FindTagCloseStandin );
+
+			private static void ValidateFindTagValue( string findTag, int lineNumber )
+			{
+				if ( !ScriptTemplateFindStrings.LookUpTable.Contains( findTag ) )
+				{
+					ExceptionLogger.LogAndThrow( new TokenValueException( $"A template string contains an unrecognized \"find\" tag value ( \"{findTag}\" )." ), lineNumber );
+				}
+			}
+
+		}
+
+
 		/// <summary>
 		/// Converts <see cref="Tokens.Find"/> token values into find-and-replace dictionary keys.
 		/// </summary>
@@ -27,7 +195,7 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 		public static ProcessedRegionData<ScriptMacroTemplate> FindTokenHandler( IndexedString[] regionData, int tokenStartIndex, ScriptMacroTemplate result )
 		{
 			var token = new StringToken( regionData[ tokenStartIndex ] );
-			result.FindAndReplace = ParseFindKeysFromStringToken( token );
+			result.FindAndReplace = FindAndReplaceParseHandler.ParseFindKeys( token );
 			return new ProcessedRegionData<ScriptMacroTemplate>( result );
 		}
 
@@ -49,7 +217,7 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 		public static ProcessedRegionData<ScriptMacroTemplate> ReplaceTokenHandler( IndexedString[] regionData, int tokenStartIndex, ScriptMacroTemplate result )
 		{
 			var token = new StringToken( regionData[ tokenStartIndex ] );
-			result.FindAndReplace = ParseReplaceValuesFromStringToken( token, result );
+			result.FindAndReplace = FindAndReplaceParseHandler.ParseReplaceValues( token, result.FindAndReplace );
 			return new ProcessedRegionData<ScriptMacroTemplate>( result );
 		}
 
@@ -72,133 +240,8 @@ namespace Petrichor.ShortcutScriptGeneration.Utilities
 		public static ProcessedRegionData<ScriptMacroTemplate> TemplateTokenHandler( IndexedString[] regionData, int tokenStartIndex, ScriptMacroTemplate result )
 		{
 			var token = new StringToken( regionData[ tokenStartIndex ] );
-			result.TemplateString = ParseTemplateFromStringToken( token );
+			result.TemplateString = TemplateParseHandler.ParseTemplateString( token );
 			return new ProcessedRegionData<ScriptMacroTemplate>( result );
-		}
-
-
-		private static string ConvertTemplateToAutoHotkeySyntax( string template, int lineNumber )
-		{
-			var components = template.Split( ControlSequences.TemplateFindReplaceDivider );
-			var doesFindStringExist = components[ 0 ]?.Length > 0;
-			var doesReplaceStringExist = components[ 1 ]?.Length > 0;
-			var isTemplateInValidFormat = doesFindStringExist && doesReplaceStringExist;
-			if ( !isTemplateInValidFormat )
-			{
-				ExceptionLogger.LogAndThrow( new TokenValueException( $"A(n) \"{Tokens.Template.Key}\" token's value is not a valid template string." ), lineNumber );
-			}
-
-			var findString = components[ 0 ].Trim();
-			var replaceString = components[ 1 ].Trim();
-			return $"::{findString}::{replaceString}";
-		}
-
-		private static string ExtractFindTagFromLine( string line, int lineNumber )
-		{
-			var lengthOfFindString = GetLengthOfFindTag( line, lineNumber );
-			return line[ ..lengthOfFindString ];
-		}
-
-		private static int GetIndexOfNextFindTagCloseChar( string line, int lineNumber )
-		{
-			var nextCloseCharIndex = line.IndexOf( Common.Syntax.ControlSequences.FindTagClose );
-			var nextOpenCharIndex = line.IndexOf( Common.Syntax.ControlSequences.FindTagOpen );
-
-			var hasNoCloseChar = nextCloseCharIndex < 0;
-			var hasMismatchedOpenChar = nextOpenCharIndex >= 0 && nextCloseCharIndex > nextOpenCharIndex;
-			if ( hasNoCloseChar || hasMismatchedOpenChar )
-			{
-				ExceptionLogger.LogAndThrow( new TokenValueException( $"A template contained a mismatched \"find\" tag open character ( '{Common.Syntax.ControlSequences.FindTagOpen}' )." ), lineNumber );
-			}
-
-			var isCloseCharEscaped = line[ nextCloseCharIndex - 1 ] == Common.Syntax.ControlSequences.Escape;
-			if ( isCloseCharEscaped )
-			{
-				var substring = line[ ( nextCloseCharIndex + 1 ).. ];
-				return nextCloseCharIndex + GetIndexOfNextFindTagCloseChar( substring, lineNumber );
-			}
-
-			return nextCloseCharIndex;
-		}
-
-		private static int GetLengthOfFindTag( string findTag, int lineNumber )
-		{
-			var findTagOpenCharTrimmed = findTag[ 1.. ];
-			var findTagValueLength = GetIndexOfNextFindTagCloseChar( findTagOpenCharTrimmed, lineNumber );
-			return findTagValueLength + 2;
-		}
-
-		private static Dictionary<string, string> ParseFindKeysFromStringToken( StringToken token )
-		{
-			var result = new Dictionary<string, string>();
-			return result;
-		}
-
-		private static Dictionary<string, string> ParseReplaceValuesFromStringToken( StringToken token, ScriptMacroTemplate result )
-		{
-			return result.FindAndReplace;
-		}
-
-		private static string ParseTemplateFromStringToken( StringToken token )
-		{
-			var rawHotstring = ConvertTemplateToAutoHotkeySyntax( token.Value, token.LineNumber );
-			var sanitizedHotstring = SanitizeHotstring( rawHotstring );
-
-			var template = new StringBuilder();
-			for ( var i = 0 ; i < sanitizedHotstring.Length ; ++i )
-			{
-				var c = sanitizedHotstring[ i ];
-
-				if ( c == Common.Syntax.ControlSequences.FindTagClose )
-				{
-					ExceptionLogger.LogAndThrow( new TokenValueException( $"A template contained a mismatched find-string close character ('{Common.Syntax.ControlSequences.FindTagClose}')." ), token.LineNumber );
-					break;
-				}
-
-				else if ( c == Common.Syntax.ControlSequences.FindTagOpen )
-				{
-					var substring = sanitizedHotstring[ i.. ];
-					var findString = ExtractFindTagFromLine( substring, token.LineNumber );
-					ValidateFindTagValue( findString, token.LineNumber );
-					_ = template.Append( findString );
-					i += findString.Length - 1;
-				}
-
-				else if ( c == Common.Syntax.ControlSequences.Escape )
-				{
-					try
-					{
-						_ = template.Append( sanitizedHotstring[ i..( i + 2 ) ] );
-						++i;
-						continue;
-					}
-					catch ( Exception exception )
-					{
-						ExceptionLogger.LogAndThrow( new TokenValueException( $"A template contained a dangling escape character ('{Common.Syntax.ControlSequences.Escape}') with no following character to escape.", exception ), token.LineNumber );
-						break;
-					}
-				}
-
-				else
-				{
-					_ = template.Append( c );
-				}
-			}
-			return template.ToString();
-		}
-
-		private static string SanitizeHotstring( string rawHotstring )
-			=> rawHotstring
-				.Replace( $"{Common.Syntax.ControlSequences.Escape}{Common.Syntax.ControlSequences.Escape}", Common.Syntax.ControlSequences.EscapeStandin )
-				.Replace( $"{Common.Syntax.ControlSequences.Escape}{Common.Syntax.ControlSequences.FindTagOpen}", Common.Syntax.ControlSequences.FindTagOpenStandin )
-				.Replace( $"{Common.Syntax.ControlSequences.Escape}{Common.Syntax.ControlSequences.FindTagClose}", Common.Syntax.ControlSequences.FindTagCloseStandin );
-
-		private static void ValidateFindTagValue( string findTag, int lineNumber )
-		{
-			if ( !ScriptTemplateFindStrings.LookUpTable.Contains( findTag ) )
-			{
-				ExceptionLogger.LogAndThrow( new TokenValueException( $"A template string contains an unrecognized \"find\" tag value ( \"{findTag}\" )." ), lineNumber );
-			}
 		}
 	}
 }
