9f77ecb272a7a918851300e2ce90b8c7f3a253d3
[Colleen Foster (she/her)] SparkliTwizzl <github@sparklitwizzl.com>
2024-04-04 23:17:28 -0600

    update lexer and docker image

--- packages/petrichor-script-lexer/petrichor_script_lexer/petrichorscriptlexer.py
+++ packages/petrichor-script-lexer/petrichor_script_lexer/petrichorscriptlexer.py
@@ -17,7 +17,7 @@ class PetrichorScriptLexer(RegexLexer):
 			(r'//.*\n', Comment.Single),
 			(r'[\{\},]', Operator),
 			(r'::', Operator),
-			(r'([a-z\-]+)(\s*)(:(?!:))', bygroups(Keyword.Reserved, Text, Operator)),
+			(r'([a-z][a-z0-9\-]*)(\s*)(:(?!:))', bygroups(Keyword.Reserved, Text, Operator)),
 			(r'\[[a-z\-]+\]', Name.Variable),
 			(r'".*"', String),
 			(r'[0-9]+', Number),
