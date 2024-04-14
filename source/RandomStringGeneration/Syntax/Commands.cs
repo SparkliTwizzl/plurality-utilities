9c779761530b03ddb4b1833e135b8282da04e0b7
[Celeste Martin (they/any)] SparkliTwizzl <github@sparklitwizzl.com>
2024-04-14 16:02:24 -0600

    fix bug by adding defaults to terminal command options
    i also bundled the module command default values into a sub-struct

--- source/RandomStringGeneration/Syntax/Commands.cs
+++ source/RandomStringGeneration/Syntax/Commands.cs
@@ -11,16 +11,20 @@
 			public const string StringCount = "--stringCount";
 			public const string StringLength = "--stringLength";
 
-			public const string AllowedCharactersDefaultValue = "abcdefghijklmnopqrstuvwxyz";
-			public const int StringCountDefaultValue = 1;
-			public const int StringLengthDefaultValue = 10;
-
 			public static Dictionary<string, string> LookUpTable => new()
 			{
 				{ Tokens.AllowedCharacters.Key, AllowedCharacters },
 				{ Tokens.StringCount.Key, StringCount },
 				{ Tokens.StringLength.Key, StringLength },
 			};
+
+
+			public readonly struct Defaults
+			{
+				public const string AllowedCharactersValue = "abcdefghijklmnopqrstuvwxyz";
+				public const int StringCountValue = 1;
+				public const int StringLengthValue = 10;
+			}
 		}
 	}
 }
