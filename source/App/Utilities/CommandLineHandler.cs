e107c9e43cdb34083bd0c514906ebbbfc3dd6e45
SparkliTwizzl <github@sparklitwizzl.com>
2024-06-19 16:20:21 -0600

    [Kiersten (she/they)] slight refactor in command line handler

--- source/App/Utilities/CommandLineHandler.cs
+++ source/App/Utilities/CommandLineHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Petrichor.Common.Containers;
+using Petrichor.Common.Info;
 using Petrichor.Common.Syntax;
 using Petrichor.Common.Utilities;
 using Petrichor.Logging;
@@ -13,13 +14,15 @@ namespace Petrichor.App.Utilities
 	{
 		private static Argument<string> InputFileArgument { get; } = new(
 			name: Commands.Options.InputFile,
-			description: "Path to input file." );
+			description: "Path to input file.",
+			getDefaultValue: () => Path.Combine( ProjectDirectories.InputDirectory, "input.petrichor" ) );
 
 
 		public static async Task<ModuleCommand> ParseArguments( string[] arguments )
 		{
-			if ( !WereArgumentsProvided( arguments ) )
+			if ( arguments.Length < 1 )
 			{
+				Console.WriteLine( $"Run with {Syntax.Commands.HelpOption} to see usage." );
 				return ModuleCommand.None;
 			}
 
@@ -31,23 +34,24 @@ namespace Petrichor.App.Utilities
 
 		private static RootCommand CreateTerminalCommands()
 		{
+			MetadataHandler.RegisterCommandOptions( Commands.Options.LookUpTable, Tokens.CommandOptionLookUpTable );
+
 			var rootCommand = new RootCommand( description: "Command line app with miscellaneous utilities." )
 				{
 					InputFileArgument,
 				};
-
-			MetadataHandler.RegisterCommandOptions( Commands.Options.LookUpTable, Tokens.CommandOptionLookUpTable );
-
 			rootCommand.Handler = CommandHandler.Create( async ( ParseResult parseResult ) =>
 				{
 					var inputFile = string.Empty;
 					try
 					{
 						TerminalOptions.IsAutoExitEnabled = parseResult.HasOption( TerminalOptions.AutoExit );
+
 						var inputHandler = new InputFileHandler( metadataRegionParser: MetadataHandler.CreateMetadataTokenParser() );
 						inputFile = parseResult.GetValueForArgument( InputFileArgument ) ?? string.Empty;
 						var data = inputHandler.ProcessFile( inputFile ).ToArray();
 						MetadataHandler.CommandToRun.Data = data;
+						
 						var logMode = MetadataHandler.CommandToRun.Options[ Commands.Options.LogMode ];
 						var logFile = MetadataHandler.CommandToRun.Options[ Commands.Options.LogFile ];
 						await MetadataHandler.InitalizeLogging( logMode, logFile );
@@ -68,23 +72,6 @@ namespace Petrichor.App.Utilities
 			rootCommand.AddCommand( RandomStringGeneration.Utilities.ModuleHandler.CreateTerminalCommand() );
 			rootCommand.AddCommand( ShortcutScriptGeneration.Utilities.ModuleHandler.CreateTerminalCommand() );
 			return rootCommand;
-		}
-
-		private static bool WereArgumentsProvided( string[] arguments )
-		{
-			if ( arguments.Length < 1 )
-			{
-				Console.WriteLine( $"Run with {App.Syntax.Commands.HelpOption} to see usage." );
-				return false;
-			}
-
-			return true;
-		}
-
-		public static void TryParseInputFile( string inputFile )
-		{
-			MetadataHandler.CommandToRun.Data = new InputFileHandler( MetadataHandler.CreateMetadataTokenParser() ).ProcessFile( inputFile ).ToArray();
-			MetadataHandler.CommandToRun.Options.Add( Commands.Options.InputFile, inputFile );
 		}
 	}
 }
